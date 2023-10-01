9860b1639414f3a4a1321c0497e80b85418bb7df
Muhammad Nur Hidayat <muhnurdayat@gmail.com>
2023-10-01 21:49:56 +0700

    create navigation menu

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,9 @@
 import "./styles/reset.css";
 import "./main.css";
+
+import { handleClickMenu } from "./scripts/menu";
+
+const hamburgerMenu = document.getElementsByClassName(
+  "hamburger-menu"
+)[0] as HTMLButtonElement;
+hamburgerMenu.addEventListener("click", handleClickMenu);
---
+++ src/scripts/menu.ts
@@ -0,0 +1,69 @@
+const openMenu = (menu: HTMLUListElement) => {
+  menu.classList.add("open-navigation-menu");
+};
+
+const closeMenu = (menu: HTMLUListElement) => {
+  menu.classList.remove("open-navigation-menu");
+};
+
+const handleClickMenuItem = (event: Event) => {
+  event.stopPropagation();
+
+  const menu = (event.target as HTMLAnchorElement).parentElement
+    ?.parentElement as HTMLUListElement;
+  closeMenu(menu);
+  window.removeEventListener("click", handleClickOutside);
+};
+
+const handleClickOutside = (event: Event) => {
+  event.stopPropagation();
+
+  const navEl = document.getElementsByTagName("nav")[0];
+  const clickedEl = event.target as HTMLElement;
+  const isClickedOutside = !navEl?.contains(clickedEl);
+  const menu = navEl?.lastElementChild as HTMLUListElement;
+
+  if (isClickedOutside) {
+    closeMenu(menu);
+    window.removeEventListener("click", handleClickOutside);
+  }
+};
+
+const handleClickMenu = (event: Event) => {
+  event.stopPropagation();
+
+  const menu = document.getElementsByClassName(
+    "navigation-menu"
+  )[0] as HTMLUListElement;
+  const menuItems = menu.children;
+  const isMenuOpened = menu.classList.value.includes("open-navigation-menu");
+
+  if (isMenuOpened) {
+    closeMenu(menu);
+    window.removeEventListener("click", handleClickOutside);
+  } else {
+    openMenu(menu);
+    window.addEventListener("click", handleClickOutside);
+    for (let i = 0; i < menuItems.length; i++) {
+      const menuItem = (menuItems[i] as HTMLLIElement)
+        .firstElementChild as HTMLAnchorElement;
+      menuItem.addEventListener("click", handleClickMenuItem);
+    }
+  }
+};
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth < 1024) return;
+
+  const menu = document.getElementsByClassName(
+    "navigation-menu"
+  )[0] as HTMLUListElement;
+
+  const isMenuOpened = menu.classList.value.includes("open-navigation-menu");
+  if (isMenuOpened) {
+    closeMenu(menu);
+    window.removeEventListener("click", handleClickOutside);
+  }
+});
+
+export { handleClickMenu };
