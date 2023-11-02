d5b65d7e0e02f7758301ffce2ef7aa35e8f99219
Muhammad Nur Hidayat <muhnurdayat@gmail.com>
2023-11-02 16:17:49 +0700

    setup image compressor

--- webpack.common.js
+++ webpack.common.js
@@ -20,6 +20,10 @@ const config = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
     ],
   },
   plugins: [
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,23 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
+/* eslint-disable import/no-extraneous-dependencies */
 const { merge } = require("webpack-merge");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
 const commonConfig = require("./webpack.common");
 
 const config = {
   mode: "production",
   devtool: "source-map",
+  optimization: {
+    minimizer: [
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            plugins: [["mozjpeg", { quality: 55, progressive: true }]],
+          },
+        },
+      }),
+    ],
+  },
 };
 
 module.exports = merge(commonConfig, config);
