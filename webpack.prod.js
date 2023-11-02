6dcf41469dee45f05dde9da93fcb8c9b8ae145c0
Muhammad Nur Hidayat <muhnurdayat@gmail.com>
2023-11-02 19:14:31 +0700

    add bundle analyzer

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,13 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const { merge } = require("webpack-merge");
 const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 const commonConfig = require("./webpack.common");
 
 const config = {
   mode: "production",
   devtool: "source-map",
+  plugins: [new BundleAnalyzerPlugin()],
   optimization: {
     minimizer: [
       new ImageMinimizerPlugin({
