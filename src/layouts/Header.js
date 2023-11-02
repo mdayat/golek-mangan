d349fe6111d9fa70d03670a0f823b2df467d2363
Muhammad Nur Hidayat <muhnurdayat@gmail.com>
2023-11-02 20:38:25 +0700

    add responsive hero image for various devices

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,4 +1,6 @@
 import { Navbar } from "./Navbar";
+import HeroSmallImg from "../../public/hero-small.jpg";
+import HeroMediumImg from "../../public/hero-medium.jpg";
 
 const Header = () => {
   const headerElement = document.createElement("header");
@@ -9,10 +11,25 @@ const Header = () => {
   hero.setAttribute("class", "hero");
   headerElement.appendChild(hero);
 
-  const heroImage = document.createElement("img");
-  heroImage.setAttribute("src", "./hero-image.jpg");
-  heroImage.setAttribute("alt", "");
-  hero.appendChild(heroImage);
+  const pictureEl = document.createElement("picture");
+  hero.appendChild(pictureEl);
+
+  const heroSmall = document.createElement("source");
+  heroSmall.setAttribute("media", "(max-width: 767px)");
+  heroSmall.setAttribute("srcset", HeroSmallImg);
+  heroSmall.setAttribute("type", "image/jpeg");
+  pictureEl.appendChild(heroSmall);
+
+  const heroMedium = document.createElement("source");
+  heroMedium.setAttribute("media", "(min-width: 768px)");
+  heroMedium.setAttribute("srcset", HeroMediumImg);
+  heroMedium.setAttribute("type", "image/jpeg");
+  pictureEl.appendChild(heroMedium);
+
+  const imageFallback = document.createElement("img");
+  imageFallback.setAttribute("src", HeroMediumImg);
+  imageFallback.setAttribute("alt", "");
+  pictureEl.appendChild(imageFallback);
 
   const heroText = document.createElement("h2");
   heroText.textContent = "Find restaurants blazingly fast with ";
